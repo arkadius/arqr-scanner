9a7a9f15af29349a11766a163cec1de709a76361
Arek Burdach <arek.burdach@gmail.com>
2017-03-20 00:14:34 +0100

    Image scaling

--- app/src/main/java/pl/ingensol/arqrscanner/BarcodeGraphic.java
+++ app/src/main/java/pl/ingensol/arqrscanner/BarcodeGraphic.java
@@ -36,6 +36,7 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
     private final int POINTS_IN_RECT_COUNT = 4;
     private final int POINTS_IN_RECT_XY_COUNT = POINTS_IN_RECT_COUNT * 2;
     private final int FONT_HEIGHT = 36;
+    private final float SCALE = 2.5f;
     private int mId;
 
     private Paint mRectPaint;
@@ -43,10 +44,12 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
     private Paint mTextPaint;
 
     private float[] mImagePoints;
-    private float[] mDstPoints;
-    private float[] mTranslatedPoints;
-    private float[] mRotatedPoints;
+    private float[] mTranslatedCornerPoints;
+    private float[] mRotatedCornerPoints;
+    private float[] mBarcodePoints;
+    private float[] mScaledBarcodePoints;
 
+    private Matrix mMatrix;
     private Path mPath;
 
     private volatile PresentedObject mPresentedObject;
@@ -65,10 +68,12 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
         mTextPaint.setTextSize(FONT_HEIGHT);
 
         mImagePoints = new float[POINTS_IN_RECT_XY_COUNT];
-        mDstPoints = new float[POINTS_IN_RECT_XY_COUNT];
-        mTranslatedPoints = new float[POINTS_IN_RECT_XY_COUNT];
-        mRotatedPoints = new float[POINTS_IN_RECT_XY_COUNT];
+        mBarcodePoints = new float[POINTS_IN_RECT_XY_COUNT];
+        mTranslatedCornerPoints = new float[POINTS_IN_RECT_XY_COUNT];
+        mRotatedCornerPoints = new float[POINTS_IN_RECT_XY_COUNT];
+        mScaledBarcodePoints = new float[POINTS_IN_RECT_XY_COUNT];
 
+        mMatrix = new Matrix();
         mPath = new Path();
     }
 
@@ -106,18 +111,14 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
     }
 
     private void drawObject(Canvas canvas, PresentedObject presentedObject) {
-        computeDestinationPoints(presentedObject, mDstPoints);
+        translate(presentedObject.getBarcode().cornerPoints, mTranslatedCornerPoints);
+        PointF center = findCenter(mTranslatedCornerPoints);
+        computeBarcodePoints(mTranslatedCornerPoints, mBarcodePoints, center);
         if (presentedObject.getKey() instanceof PresentedImageKey) {
-            drawImage(canvas, presentedObject);
+            drawImage(canvas, presentedObject, mBarcodePoints, center);
         } else {
-            drawRawText(canvas, presentedObject);
+            drawRawText(canvas, presentedObject, mBarcodePoints, center);
         }
-    }
-
-    private void computeDestinationPoints(PresentedObject presentedObject, float[] dst) {
-        translate(presentedObject.getBarcode().cornerPoints, mTranslatedPoints);
-        int topLeftPointXyIndex = findTopLeftPointXyIndex(mTranslatedPoints);
-        copyShifted(mTranslatedPoints, dst, topLeftPointXyIndex);
     }
 
     private void translate(Point[] src, float[] dst) {
@@ -126,33 +127,6 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
             dst[i * 2] = translateX(point.x);
             dst[i * 2 + 1] = translateY(point.y);
         }
-    }
-
-    private int findTopLeftPointXyIndex(float[] points) {
-        PointF center = findCenter(points);
-        Matrix rotationWithRespectToCenterMatrix = computeRotationWithRespectToPointMatrix(getRotationMatrix(), center);
-        rotationWithRespectToCenterMatrix.mapPoints(mRotatedPoints, points);
-
-        float minXY = Float.MAX_VALUE;
-        int topLeftPoint = -1;
-        for (int i = 0; i < POINTS_IN_RECT_COUNT; i++) {
-            float x = mRotatedPoints[i * 2];
-            float y = mRotatedPoints[i * 2 + 1];
-            float pointXY = x * x + y * y;
-            if (pointXY < minXY) {
-                minXY = pointXY;
-                topLeftPoint = i;
-            }
-        }
-
-        return topLeftPoint * 2;
-    }
-
-    private Matrix computeRotationWithRespectToPointMatrix(Matrix rotationMatrix, PointF center) {
-        Matrix rotationWithRespectToCenterMatrix = new Matrix(rotationMatrix);
-        rotationWithRespectToCenterMatrix.preTranslate(-center.x, -center.y);
-        rotationWithRespectToCenterMatrix.postTranslate(center.x, center.y);
-        return rotationWithRespectToCenterMatrix;
     }
 
     private PointF findCenter(float[] points) {
@@ -167,13 +141,39 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
         return new PointF(xSum / POINTS_IN_RECT_COUNT, ySum / POINTS_IN_RECT_COUNT);
     }
 
+    private void computeBarcodePoints(float[] src, float[] dst, PointF center) {
+        int topLeftPointXyIndex = findTopLeftPointXyIndex(src, center);
+        copyShifted(src, dst, topLeftPointXyIndex);
+    }
+
+    private int findTopLeftPointXyIndex(float[] points, PointF center) {
+        mMatrix.reset();
+        mMatrix.set(getRotationMatrix());
+        transformRespectToPoint(mMatrix, center);
+        mMatrix.mapPoints(mRotatedCornerPoints, points);
+
+        float minXY = Float.MAX_VALUE;
+        int topLeftPoint = -1;
+        for (int i = 0; i < POINTS_IN_RECT_COUNT; i++) {
+            float x = mRotatedCornerPoints[i * 2];
+            float y = mRotatedCornerPoints[i * 2 + 1];
+            float pointXY = x * x + y * y;
+            if (pointXY < minXY) {
+                minXY = pointXY;
+                topLeftPoint = i;
+            }
+        }
+
+        return topLeftPoint * 2;
+    }
+
     private void copyShifted(float[] src, float[] dst, int shift) {
         int pointsFromShiftToEnd = POINTS_IN_RECT_XY_COUNT - shift;
         System.arraycopy(src, shift, dst, 0,                    pointsFromShiftToEnd);
         System.arraycopy(src, 0,     dst, pointsFromShiftToEnd, shift);
     }
 
-    private void drawImage(Canvas canvas, PresentedObject presentedImage) {
+    private void drawImage(Canvas canvas, PresentedObject presentedImage, float[] barcodePoints, PointF center) {
         Bitmap bitmap = (Bitmap) presentedImage.getLoadedValue();
 
         mImagePoints[0] = 0;
@@ -188,25 +188,47 @@ public class BarcodeGraphic extends GraphicOverlay.Graphic {
         mImagePoints[6] = 0;
         mImagePoints[7] = bitmap.getHeight();
 
-        Matrix matrix = new Matrix();
-        matrix.setPolyToPoly(mImagePoints, 0, mDstPoints, 0, POINTS_IN_RECT_COUNT);
-        canvas.drawBitmap(bitmap, matrix, mImagePaint);
+        scalePointsWithRespectToPointKeepingRatio(barcodePoints, mScaledBarcodePoints, center, bitmap.getWidth(), bitmap.getHeight(), SCALE);
+
+        mMatrix.reset();
+        mMatrix.setPolyToPoly(mImagePoints, 0, mScaledBarcodePoints, 0, POINTS_IN_RECT_COUNT);
+
+        canvas.drawBitmap(bitmap, mMatrix, mImagePaint);
     }
 
-    private void drawRawText(Canvas canvas, PresentedObject presentedText) {
+    private void drawRawText(Canvas canvas, PresentedObject presentedText, float[] barcodePoints, PointF center) {
+        scalePointsWithRespectToPointKeepingRatio(barcodePoints, mScaledBarcodePoints, center, 1, 1, SCALE);
+
         mPath.reset();
-        mPath.moveTo(mDstPoints[0], mDstPoints[1]);
+        mPath.moveTo(mScaledBarcodePoints[0], mScaledBarcodePoints[1]);
 
         for (int i = 0; i < POINTS_IN_RECT_COUNT; i++) {
             mPath.lineTo(
-                    mDstPoints[(i + 1) % POINTS_IN_RECT_XY_COUNT],
-                    mDstPoints[(i + 1) % POINTS_IN_RECT_XY_COUNT + 1]);
+                    mScaledBarcodePoints[(i + 1) % POINTS_IN_RECT_COUNT * 2],
+                    mScaledBarcodePoints[(i + 1) % POINTS_IN_RECT_COUNT * 2 + 1]);
         }
         canvas.drawPath(mPath, mRectPaint);
 
         Rect rect = presentedText.getBarcode().getBoundingBox();
         PresentedTextKey key = (PresentedTextKey) presentedText.getKey();
         canvas.drawText(key.getText(), translateX(rect.left), translateY(rect.top + FONT_HEIGHT), mTextPaint);
+    }
+
+    private void scalePointsWithRespectToPointKeepingRatio(float[] src, float[] dst, PointF point, int width, int height, float scale) {
+        float scaleBase = Math.min(width, height);
+        float scaleX = width / scaleBase * scale;
+        float scaleY = height / scaleBase * scale;
+
+        mMatrix.reset();
+        mMatrix.setScale(scaleX, scaleY);
+        transformRespectToPoint(mMatrix, point);
+
+        mMatrix.mapPoints(dst, src);
+    }
+
+    private void transformRespectToPoint(Matrix matrix, PointF point) {
+        matrix.preTranslate(-point.x, -point.y);
+        matrix.postTranslate(point.x, point.y);
     }
 
 }
