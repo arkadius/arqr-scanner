6156857f7b871622b6d8ae58e8785677e59d61ac
Arek Burdach <arek.burdach@gmail.com>
2017-03-20 09:50:21 +0100

    Extracted LoadedValueMemo from TrackedFactory

--- app/src/main/java/pl/ingensol/arqrscanner/LoadedValueMemo.java
+++ app/src/main/java/pl/ingensol/arqrscanner/LoadedValueMemo.java
@@ -1,7 +1,54 @@
 package pl.ingensol.arqrscanner;
 
-interface LoadedValueMemo {
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 
-    Object getLoadedValue(PresentedObjectKey key);
+import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+class LoadedValueMemo {
+
+    private Map<PresentedObjectKey, Object> mLoadedValues = new HashMap<>();
+
+    synchronized Object getLoadedValue(PresentedObjectKey key) {
+        Object value = mLoadedValues.get(key);
+        if (value == null) {
+            value = loadValue(key);
+        }
+        if (value != null) {
+            mLoadedValues.put(key, value);
+        }
+        return value;
+    }
+
+    private Object loadValue(PresentedObjectKey key) {
+        if (key instanceof PresentedImageKey) {
+            try {
+                PresentedImageKey presentedImage = (PresentedImageKey) key;
+                return new DownloadImageTask().execute(presentedImage.getUrl()).get();
+            } catch (Exception e) {
+                Log.e("barcode", "Image downloading execution exception", e);
+            }
+        }
+        return null;
+    }
+
+    private static class DownloadImageTask extends AsyncTask<URL, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(URL... urls) {
+            URL url = urls[0];
+            Log.i("barcode", "Loading image: " + url);
+            try {
+                return BitmapFactory.decodeStream(url.openStream());
+            } catch (IOException e) {
+                Log.e("barcode", "Invalid image stream", e);
+                return null;
+            }
+        }
+    }
 
 }
